b6b44ff9e9e64d747493eead471b9d573a54bc89
SkarnerV <skarner.han@gmail.com>
2024-03-06 13:30:36 -0500

    removed rotation text

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div className="flex flex-col bg-cover bg-mainbg h-screen items-center w-full">
       <div className=" text-center text-white font-bold text-4xl md:text-6xl font-mono mt-64">
-        <RotatingText />
+        {/* <RotatingText /> */}
       </div>
       <div className="mt-6"></div>
     </div>
